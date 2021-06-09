9cd35b409a8860a7c85943a8dd6dd4072b2ea626
Waqas Kayani <mvickykayani@gmail.com>
2021-06-09 14:28:03 +0500

    updated terraform scripts

--- dynamodb.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "state_lock_table" {
-    name = "state-lock-table"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
-}
