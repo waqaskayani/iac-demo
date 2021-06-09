85f78d274565f41f5e3b0b97b7fb348cf7e6c9af
Waqas Kayani <mvickykayani@gmail.com>
2021-06-09 11:23:42 +0500

    updated variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "vpc_cird" {
+variable "vpc_cidr" {
     type        = string
     description = "This is an example input variable that will set CIDR range of VPC."
 }
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cird
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
 
   tags = {
