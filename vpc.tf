6689635a7c0d6a41c9894a6ea5d7001b34790797
Waqas Kayani <mvickykayani@gmail.com>
2021-06-09 13:54:05 +0500

    updated terraform scripts

--- vpc.tf
+++ vpc.tf
@@ -5,8 +5,8 @@ module "vpc" {
   cidr = var.vpc_cidr
 
   azs             = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["${cidrsubnet(var.vpc_cidr, 8, 1)}", "${cidrsubnet(var.vpc_cidr, 8, 2)}", "${cidrsubnet(var.vpc_cidr, 8, 3)}"]
+  public_subnets  = ["${cidrsubnet(var.vpc_cidr, 8, 101)}", "${cidrsubnet(var.vpc_cidr, 8, 102)}", "${cidrsubnet(var.vpc_cidr, 8, 103)}"]
 
   enable_nat_gateway = true
   single_nat_gateway  = true
