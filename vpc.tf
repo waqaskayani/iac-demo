e77fbc4265b59609adba3ecc2867366b5a28202e
Waqas Kayani <mvickykayani@gmail.com>
2021-06-09 13:56:00 +0500

    updated terraform scripts

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "EKS-VPC"
+  name = "Velocidata-EKS-VPC"
   cidr = var.vpc_cidr
 
   azs             = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
