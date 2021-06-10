e08c99b831359593f0336330fbc9bda3ee660f49
Waqas Kayani <mvickykayani@gmail.com>
2021-06-10 14:44:15 +0500

    updated terraform scripts for eks

--- eks.tf
+++ eks.tf
@@ -9,29 +9,31 @@ data "aws_eks_cluster_auth" "cluster" {
 module "eks" {
     source          = "terraform-aws-modules/eks/aws"
     cluster_name    = "vd-cluster"
-    cluster_version = "1.17"
+    cluster_version = "1.20.4"
     subnets         = data.aws_subnet_ids.private_subnets.ids
     vpc_id          = module.vpc.vpc_id
 
-    node_groups = {
-        private = {
-            subnets          = data.aws_subnet_ids.private_subnets.ids
-            desired_capacity = 1
-            max_capacity     = 3
-            min_capacity     = 1
-
-            name_prefix      = "staging-vd-workers-"
-            instance_types   = ["t2.small"]
-            k8s_labels = {
-                Environment  = "private"
-            }
-
-            tags = {
-                Environment  = "staging"
-                Organization = "Emumba"
-            }
+    worker_groups = [
+        {
+            name                 = "staging-vd-workers"
+            instance_type        = "t2.small"
+            asg_desired_capacity = 1
+            asg_max_size         = 3
+            asg_min_size         = 1
+            tags = [
+                {
+                    "key"                 = "Environment"
+                    "propagate_at_launch" = "true"
+                    "value"               = "staging"
+                },
+                {
+                    "key"                 = "Organization"
+                    "propagate_at_launch" = "true"
+                    "value"               = "Emumba"
+                }
+            ]
         }
-    }
+    ]
 
     map_users = [
         {
---
+++ rds.tf
@@ -0,0 +1,46 @@
+resource "aws_db_subnet_group" "default" {
+    name       = "subnet-group"
+    subnet_ids = data.aws_subnet_ids.private_subnets.ids
+
+    tags = {
+        Name = "subnet-group"
+    }
+}
+
+resource "random_password" "password" {
+    length           = 12
+    special          = true
+    override_special = "-_%"
+}
+
+resource "aws_db_instance" "app_db" {
+    identifier           = "velocidata-stage-postgres"
+    allocated_storage    = 20
+    storage_type         = "gp2"
+    engine               = "postgres"
+    engine_version       = "12.5"
+    instance_class       = "db.t2.medium"   # change to db.m6g.large
+    name                 = "postgres"
+    username             = "postgres"
+    password             = random_password.password.result
+    publicly_accessible  = false
+    multi_az             = false
+    skip_final_snapshot  = false
+    final_snapshot_identifier = "velocidata-stage-postgres-final-snapshot"
+    deletion_protection  = false
+    enabled_cloudwatch_logs_exports = ["postgresql","upgrade"]
+    auto_minor_version_upgrade = false
+    db_subnet_group_name = aws_db_subnet_group.default.id
+    vpc_security_group_ids = [module.eks.cluster_primary_security_group_id, module.eks.cluster_security_group_id]
+
+    tags = {
+            Name = "velocidata-stage-postgres"
+    }
+}
+
+resource "aws_ssm_parameter" "VD_DB_PASSWORD" {
+    name  = "/Velocidata/APP/DB_PASSWORD"
+    type  = "SecureString"
+    value = random_password.password.result
+    overwrite = true
+}
