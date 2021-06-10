b1ab6fa68606d95636ab3619cc23f391dc3e3577
Waqas Kayani <mvickykayani@gmail.com>
2021-06-10 19:21:32 +0500

    updated terraform scripts for eks

--- data.tf
+++ data.tf
@@ -14,8 +14,11 @@ data "aws_ami" "ubuntu" {
     
     filter {
         name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
     }
     
-    owners = ["099720109477"]
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 }
