05fecff3561524ca8f54bd2cb9bf19194a5fc38b
Waqas Kayani <mvickykayani@gmail.com>
2021-06-11 13:39:03 +0500

    updated terraform scripts for eks

--- vpc.tf
+++ vpc.tf
@@ -36,6 +36,13 @@ resource "aws_security_group" "wireguard_sg" {
     cidr_blocks = ["182.191.83.208/32"]
   }
 
+  ingress {
+    from_port   = 54321
+    to_port     = 54321
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
