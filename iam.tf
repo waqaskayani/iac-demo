ac15002f3ca94849b0b8de26fed4202cfbfc020a
Waqas Kayani <mvickykayani@gmail.com>
2021-06-16 02:01:54 +0500

    updated terraform scripts for eks

--- iam.tf
+++ iam.tf
@@ -60,42 +60,8 @@ resource "aws_iam_role_policy_attachment" "policy_attach" {
 #########################
 ####### Role for Workers
 #########################
-resource "aws_iam_role" "role_for_workers" {
-    name = "role-for-workers"
-
-    assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Sid": "EKSWorkerAssumeRole",
-        "Effect": "Allow",
-        "Principal": {
-            "Service": "ec2.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-        }
-    ]
-}
-EOF
-
-    tags = {
-            Name = "role-for-workers"
-        }
-}
-
-data "aws_iam_policy" "AmazonEKSWorkerNodePolicy" {
-    arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-data "aws_iam_policy" "AmazonEC2ContainerRegistryReadOnly" {
-    arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-data "aws_iam_policy" "AmazonEKS_CNI_Policy" {
-    arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_policy" "policy_for_workers" {
-    name        = "policy-for-workers"
+resource "aws_iam_policy" "workers_autoscaling_policy" {
+    name        = "workers-autoscaling-policy"
     policy      = <<EOF
 {
     "Version": "2012-10-17",
@@ -116,21 +82,4 @@ resource "aws_iam_policy" "policy_for_workers" {
     ]
 }
 EOF
-}
-
-resource "aws_iam_role_policy_attachment" "policy_attach_1" {
-    role      = aws_iam_role.role_for_workers.name
-    policy_arn = aws_iam_policy.policy_for_workers.arn
-}
-resource "aws_iam_role_policy_attachment" "policy_attach_2" {
-    role      = aws_iam_role.role_for_workers.name
-    policy_arn = data.aws_iam_policy.AmazonEKSWorkerNodePolicy.arn
-}
-resource "aws_iam_role_policy_attachment" "policy_attach_3" {
-    role      = aws_iam_role.role_for_workers.name
-    policy_arn = data.aws_iam_policy.AmazonEC2ContainerRegistryReadOnly.arn
-}
-resource "aws_iam_role_policy_attachment" "policy_attach_4" {
-    role      = aws_iam_role.role_for_workers.name
-    policy_arn = data.aws_iam_policy.AmazonEKS_CNI_Policy.arn
 }
