cd88e2d018b716d2650c22ee22c88e4edae968ac
Waqas Kayani <mvickykayani@gmail.com>
2021-06-15 14:56:03 +0500

    updated terraform scripts for eks

--- eks.tf
+++ eks.tf
@@ -76,4 +76,5 @@ resource "helm_release" "ebs_csi" {
     name       = "aws-ebs-csi-driver"
     repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
     chart      = "aws-ebs-csi-driver"
+    namespace  = "kube-system"
 }
