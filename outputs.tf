e0a7f0d1cdf85caf7f0a2b39455a198479c4f22f
Waqas Kayani <mvickykayani@gmail.com>
2021-06-16 14:14:40 +0500

    updated terraform scripts for eks

--- outputs.tf
+++ outputs.tf
@@ -9,19 +9,3 @@ output "instance_public_ip" {
 output "worker_asg_name" {
     value = module.eks.workers_asg_names
 }
-
-output "hcl" {
-    value = "${yamldecode(file("pod.yaml"))}"
-}
-
-/* output "rendered" {
-    value = "${data.template_file.deployment.rendered}"
-}
-
-data "template_file" "deployment" {
-    template = "${file("pod.yaml")}"
-
-    vars {
-        replicas = 3
-    }
-} */
