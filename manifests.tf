3cc7d256cc1879ffea5da6647f3b9761ea9de6bc
Waqas Kayani <mvickykayani@gmail.com>
2021-06-16 14:08:23 +0500

    updated terraform scripts for eks

--- manifests.tf
+++ manifests.tf
@@ -1,6 +1,6 @@
-resource "kubectl_manifest" "pod" {
+/* resource "kubectl_manifest" "pod" {
     yaml_body = file("pod.yaml")
 }
 resource "kubectl_manifest" "deployment" {
     yaml_body = file("deployment.yaml")
-}
+} */
