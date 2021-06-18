d0ebab46acf63930de3698d350bdc2ee1b8e5807
Waqas Kayani <mvickykayani@gmail.com>
2021-06-18 15:30:21 +0500

    update eks scripts

--- manifests.tf
+++ manifests.tf
@@ -1,8 +1,8 @@
-/* resource "kubectl_manifest" "deployment" {
-    yaml_body = file("deployment.yaml")
+resource "kubectl_manifest" "terraform_deployment" {
+    yaml_body =  file("deployment.yaml")
 }
 
-
+/* 
 resource "kubectl_manifest" "service" {
     yaml_body = file("service.yaml")
 } */
