87365b320d50cff217c2fe21b3e60e1a2dd692aa
Waqas Kayani <mvickykayani@gmail.com>
2021-06-21 20:54:16 +0500

    update terraform rds scripts

--- provider.tf
+++ provider.tf
@@ -26,9 +26,14 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-    host                   = data.aws_eks_cluster.cluster[0].endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster[0].certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster[0].token
+    host                   = module.eks[0].endpoint
+    cluster_ca_certificate = base64decode(module.eks[0].cluster_certificate_authority_data)
+    exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+    command     = "aws"
+    }
+    /* token                  = data.aws_eks_cluster_auth.cluster[0].token */
     load_config_file       = false
 }
 
